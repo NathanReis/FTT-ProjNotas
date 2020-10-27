4145101636e04870148eb3e3ba5cffd0f429848f
Vitor Lupinetti <vitorlupinetti@MacBook-Air-de-Vitor.local>
2020-10-26 23:10:23 -0300

    pagina home

--- N2-Web/my-app/src/pages/Home/Home.tsx
+++ N2-Web/my-app/src/pages/Home/Home.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import '../../styles/Home.css'
+import User from '../../assets/user.png';
+import HomeImage from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
-const Home = ()=> {
-    return(
+const Home = () => {
+    return (
         <div id="homepage-container">
-            
-            <div className="side-menu">
-                
-            </div>
-            <div>
 
+            <div className="side-menu">
+                <div className="side-container">
+                    <img alt="user" src={User} />
+                    <h2>Bem vindo usuário</h2>
+                    <Link to="">Escolher matérias</Link>
+                    <Link to="">Cadastrar notas</Link>
+                    <Link to="">Ver médias</Link>
+                    <Link to="">Editar conta</Link>
+                </div>
+            </div>
+            <div className="home-container">
+                <h1>Projeto notas</h1>
+                <p>(precisamos pensar em um nome melhor)</p>
+                <div className="image-container">
+                    <img src={HomeImage} />
+                    <ul>
+                        <li>Escolha suas matérias que está cursando na faculdade</li>
+                        <li>Informe sua média nessa matéria</li>
+                        <li>Veja como está se saindo em relação à sua turma (anonimamente)</li>
+                        <li>Veja como está se saindo em relação à outras instituições (anonimamente)</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
