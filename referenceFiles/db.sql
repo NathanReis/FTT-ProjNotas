91bf63c09f7a50b337c7b2d1fc6338e4d9df6f8a
Vitor Lupinetti <vi.lupinetti@hotmail.com>
2020-11-03 23:05:45 -0300

    correção arquivo banco de dados dnv

--- referenceFiles/db.sql
+++ referenceFiles/db.sql
@@ -4,7 +4,7 @@ USE `projnotas`;
 
 DROP USER IF EXISTS 'es2lp3n2'@'localhost';
 CREATE USER 'es2lp3n2'@'localhost' IDENTIFIED BY 'nsvw@#220Z';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER ON `projnotas` . * TO 'es2lp3n2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `projnotas` . * TO 'es2lp3n2'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE TABLE `tbTeachingInstitutions`
