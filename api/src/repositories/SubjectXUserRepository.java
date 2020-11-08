ce8c5b528df61a582b4fa3a28c1bda80da39fd0a
Nathan Reis <nathansilvareis@gmail.com>
2020-11-08 16:05:47 -0300

    Camada repository de subject x user

---
+++ api/src/repositories/SubjectXUserRepository.java
@@ -0,0 +1,86 @@
+package repositories;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import models.SubjectModel;
+import models.SubjectXUserModel;
+import models.TeachingInstitutionModel;
+import models.UserModel;
+
+public class SubjectXUserRepository extends Repository<SubjectXUserModel> {
+	public SubjectXUserRepository() {
+		super("tbSubjectsXUsers");
+	}
+
+	@Override
+	public int create(SubjectXUserModel entity) throws SQLException, ClassNotFoundException {
+		String sql = "INSERT INTO " + this.table + " ";
+		sql       += "  (idSubject, idTeachingInstitution, idUser, grade, semester, year) ";
+		sql       += "VALUES ";
+		sql       += "  (?, ?, ?, ?, ?, ?);";
+		
+		try(PreparedStatement stmt = ConnectionDB.getInstance().prepareStatement(sql)) {
+			stmt.setInt(1, entity.getSubject().getId());
+			stmt.setInt(2, entity.getTeachingInstitution().getId());
+			stmt.setInt(3, entity.getUser().getId());
+			stmt.setDouble(4, entity.getGrade());
+			stmt.setInt(5, entity.getSemester());
+			stmt.setInt(6, entity.getYear());
+			
+			stmt.executeUpdate();
+		}
+		
+		return this.getInsertedId();
+	}
+	
+	@Override
+	public SubjectXUserModel fillModel(ResultSet resultSet) throws SQLException {
+		SubjectModel subjectModel = new SubjectModel();
+		subjectModel.setId(resultSet.getInt("idSubject"));
+		
+		TeachingInstitutionModel teachingInstitutionModel = new TeachingInstitutionModel();
+		teachingInstitutionModel.setId(resultSet.getInt("idTeachingInstitution"));
+		
+		UserModel userModel = new UserModel();
+		userModel.setId(resultSet.getInt("idUser"));
+		
+		SubjectXUserModel subjectXUserModel = new SubjectXUserModel();
+		subjectXUserModel.setId(resultSet.getInt("id"));
+		subjectXUserModel.setSubject(subjectModel);
+		subjectXUserModel.setTeachingInstitution(teachingInstitutionModel);
+		subjectXUserModel.setUser(userModel);
+		subjectXUserModel.setGrade(resultSet.getDouble("grade"));
+		subjectXUserModel.setSemester(resultSet.getInt("semester"));
+		subjectXUserModel.setYear(resultSet.getInt("year"));
+		
+		return subjectXUserModel;
+	}
+
+	@Override
+	public void update(SubjectXUserModel entity) throws SQLException, ClassNotFoundException {
+		String sql = "UPDATE " + this.table + " ";
+		sql       += "SET ";
+		sql       += "  idSubject = ?, ";
+		sql       += "  idTeachingInstitution = ?, ";
+		sql       += "  idUser = ?, ";
+		sql       += "  grade = ?, ";
+		sql       += "  semester = ?, ";
+		sql       += "  year = ? ";
+		sql       += "WHERE ";
+		sql       += "  id = ?;";
+		
+		try(PreparedStatement stmt = ConnectionDB.getInstance().prepareStatement(sql)) {
+			stmt.setInt(1, entity.getSubject().getId());
+			stmt.setInt(2, entity.getTeachingInstitution().getId());
+			stmt.setInt(3, entity.getUser().getId());
+			stmt.setDouble(4, entity.getGrade());
+			stmt.setInt(5, entity.getSemester());
+			stmt.setInt(6, entity.getYear());
+			stmt.setInt(7, entity.getId());
+			
+			stmt.executeUpdate();
+		}
+	}
+}
